efc91c6625c37daf2fe723d10c560f21ab13bc6a
Fritz Brenner Flores Yarasca <fritz.flores.27@unsch.edu.pe>
2022-06-28 11:01:24 -0500

    Agregamos imágen y movimiento

--- Clases/claseGato.java
+++ Clases/claseGato.java
@@ -1,7 +1,65 @@
 package Clases;
-public class claseGato{
-int x;
-int y;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class claseGato extends JPanel implements KeyListener{
+//OBTENEMOS LA IMAGEN
+    File file = new File("D:\\git-PROYECTO MANGUITO\\Imagenes\\imagen.jpg");
+    BufferedImage bufferedImage = ImageIO.read(file);
+    ImageIcon imageIcon = new ImageIcon(bufferedImage);
+    JLabel ImagenGato = new JLabel(imageIcon);
+    
+
+//Eventos de las teclas para mover al michi y cargar la imágen con el método add()
+    public claseGato() throws IOException{
+        addKeyListener(this);
+        setFocusable(true);
+        add(ImagenGato);
+
+    }
+    @Override
+    public void keyTyped(KeyEvent e) {
+        if (e.getKeyChar() == 'w' ||e.getKeyChar() == 'w' || e.getExtendedKeyCode() == KeyEvent.VK_UP) {
+        ImagenGato.setLocation(ImagenGato.getX(), ImagenGato.getY()-10);
+        }
+        if (e.getKeyChar() == 's' ||e.getKeyChar() == 's' || e.getExtendedKeyCode() == KeyEvent.VK_DOWN) {
+            ImagenGato.setLocation(ImagenGato.getX(), ImagenGato.getY()+10);
+        }
+        if (e.getKeyChar() == 'd' ||e.getKeyChar() == 'd' || e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
+            ImagenGato.setLocation(ImagenGato.getX()+10, ImagenGato.getY());
+        }
+        if (e.getKeyChar() == 'a' ||e.getKeyChar() == 'a' || e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
+            ImagenGato.setLocation(ImagenGato.getX()-10, ImagenGato.getY());
+        }
+    }
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getExtendedKeyCode() == KeyEvent.VK_UP) {
+            ImagenGato.setLocation(ImagenGato.getX(), ImagenGato.getY()-10);
+        }
+        if (e.getExtendedKeyCode() == KeyEvent.VK_DOWN) {
+            ImagenGato.setLocation(ImagenGato.getX(), ImagenGato.getY()+10);
+        }
+        if (e.getExtendedKeyCode() == KeyEvent.VK_RIGHT) {
+            ImagenGato.setLocation(ImagenGato.getX()+10, ImagenGato.getY());
+        }
+        if (e.getExtendedKeyCode() == KeyEvent.VK_LEFT) {
+            ImagenGato.setLocation(ImagenGato.getX()-10, ImagenGato.getY());
+        }
+    }
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+    }
 
 
 }
--- ProyectoManguito.java
+++ ProyectoManguito.java
@@ -1,6 +1,24 @@
-public class ProyectoManguito {
-    public static void main(String[] args) {
-        System.out.println("Hola");
+import java.awt.Color;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import Clases.claseGato;
+
+public class ProyectoManguito extends JPanel {
+    public static void main(String[] args) throws IOException {
+        JFrame ventana = new JFrame("El mundo de Manguito");
+        //Interfaz objInterfaz = new Interfaz();
+        //claseGato objGato = new claseGato();
+
+        //ventana.add(objInterfaz);
+        ventana.getContentPane().add(new claseGato());
+        ventana.setSize(1200, 700);
+        ventana.setLocationRelativeTo(null);
+        ventana.getContentPane().setBackground(new Color(0, 255,204));
+        ventana.setVisible(true);
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 
